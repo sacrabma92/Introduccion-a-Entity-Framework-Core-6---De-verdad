737d5b59f1cdd73a95f1377c6bcae30628ee476f
Carlos Ramirez <karlossander92@gmail.com>
2023-06-30 17:40:17 -0500

    Tabla Genero, Actor, Cine, Peliculas Creadas

---
+++ EFCorePeliculas2/EFCorePeliculas2/Entidades/Actor.cs
@@ -0,0 +1,10 @@
+﻿namespace EFCorePeliculas2.Entidades
+{
+    public class Actor
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Biografia { get; set; }
+        public DateTime? FechaNacimiento { get; set; }
+    }
+}
---
+++ EFCorePeliculas2/EFCorePeliculas2/Entidades/Genero.cs
@@ -0,0 +1,9 @@
+﻿namespace EFCorePeliculas2.Entidades
+{
+    public class Genero
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+    }
+}
