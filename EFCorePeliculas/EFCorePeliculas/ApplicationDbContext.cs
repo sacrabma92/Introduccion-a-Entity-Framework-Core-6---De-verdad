2ab0a31c615fc0ea4ad266e64f2814cd1a6c12fe
Carlos Ramirez <karlossander92@gmail.com>
2023-09-16 14:17:33 -0500

    Creando proyecto y entidad genero

---
+++ EFCorePeliculas/EFCorePeliculas/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using EFCorePeliculas.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCorePeliculas
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        DbSet<Genero> Generos { get; set; }
+    }
+}
